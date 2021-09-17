888d682ed745e91412c3199e7aa70a255ca19d83
Ahmed <ahmed.a.qureshi@gmail.com>
2021-09-17 19:08:00 -0400

    List1
    A python program that produces the average grade of a student based on their test scores

---
+++ qureshi-list1.py
@@ -0,0 +1,10 @@
+test=[0, 1, 2, 3, 4, 5]
+score=[79,84,91,97,83,65]
+print "test number: ", (test[0]), "score: ", (score[0])
+print "test number: ", (test[1]), "score: ", (score[1])
+print "test number: ", (test[2]), "score: ", (score[2])
+print "test number: ", (test[3]), "score: ", (score[3])
+print "test number: ", (test[4]), "score: ", (score[4])
+print "test number: ", (test[5]), "score: ", (score[5])
+average=(score[0]+score[1]+score[2]+score[3]+score[4]+score[5])/6
+print "average: ", average
