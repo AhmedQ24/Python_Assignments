00b07a6559f7acc19c16bf3c0334c8100e800823
Ahmed <ahmed.a.qureshi@gmail.com>
2021-09-17 19:09:21 -0400

    reverseWord
    A program that prints the user input and then prints it again in reverse using while loops

---
+++ reverseword.py
@@ -0,0 +1,12 @@
+word='Ahmed'
+letters = list(word)
+count1=0
+print("the word is defined to be the characters:")
+while count1 < len(letters):
+    print(letters[count1])
+    count1 = count1 + 1
+count2=4
+print("In reverse, the characters are:")
+while count2 >= 0:
+    print(letters[count2])
+    count2 = count2 - 1
