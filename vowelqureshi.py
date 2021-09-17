1da7525b090ef98920dbbd7c1fc43cef8abd4de6
Ahmed <ahmed.a.qureshi@gmail.com>
2021-09-17 19:09:54 -0400

    isVowel
    A program that uses ifElse loops to determine if the user input char is a vowel or not

---
+++ vowelqureshi.py
@@ -0,0 +1,11 @@
+ch = input("Enter a character: ")
+
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u' or ch=='Y' or ch=='y'):
+    	print("that's a vowel")
+elif(ch=='0'):
+		exit()
+else:
+   		ch = input("Enter a character: ")
+		
